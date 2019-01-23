6c15684bd01c5cb21395fe049335722342b9a0a5
Dylan Buth <dylanbuth11@gmail.com>
2019-01-23 13:38:18 -0500

    Scanner?

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert('hey');
+navigator.usb.requestDevice({ filters: [{ vendorId: 1351 }] })
+    .then(device => {
+    console.log(device.productName);      // "Arduino Micro"
+console.log(device.manufacturerName); // "Arduino LLC"
+})
+.catch(error => { console.log(error); });
